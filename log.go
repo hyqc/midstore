655e777605333000c00e28d8df38e3a0f7241b9e
hyqc <test@qq.com>
2025-06-04 14:55:36 +0800

    更新log

--- log.go
+++ log.go
@@ -13,17 +13,17 @@ func newLog() *Log {
 }
 
 func (l *Log) Debugf(format string, v ...interface{}) {
-	fmt.Printf("[DEBUG] " + time.Now().Format(time.RFC3339) + fmt.Sprintf(format, v...) + "\n")
+	fmt.Printf("[DEBUG] " + time.Now().Format(time.RFC3339) + " " + fmt.Sprintf(format, v...) + "\n")
 }
 
 func (l *Log) Infof(format string, v ...interface{}) {
-	fmt.Printf("[INFO] " + time.Now().Format(time.RFC3339) + fmt.Sprintf(format, v...) + "\n")
+	fmt.Printf("[INFO] " + time.Now().Format(time.RFC3339) + " " + fmt.Sprintf(format, v...) + "\n")
 }
 
 func (l *Log) Warnf(format string, v ...interface{}) {
-	fmt.Printf("[WARN] " + time.Now().Format(time.RFC3339) + fmt.Sprintf(format, v...) + "\n")
+	fmt.Printf("[WARN] " + time.Now().Format(time.RFC3339) + " " + fmt.Sprintf(format, v...) + "\n")
 }
 
 func (l *Log) Errorf(format string, v ...interface{}) {
-	fmt.Printf("[ERROR] " + time.Now().Format(time.RFC3339) + fmt.Sprintf(format, v...) + "\n")
+	fmt.Printf("[ERROR] " + time.Now().Format(time.RFC3339) + " " + fmt.Sprintf(format, v...) + "\n")
 }
